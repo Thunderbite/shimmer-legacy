5ef31824a4729ae84ae4adac438dad51c355d958
Vlad Filip <vlad.filip.devl@gmail.com>
2018-10-01 15:39:46 +0300

    fixed typo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ module.exports = {
   Button: require( './button' ),
   Element: require( './element' ),
   Scene: require( './scene' ),
-  Sprite: require( './button' ),
+  Sprite: require( './sprite' ),
   Stage: require( './stage' ),
   Text: require( './button' )
 }
