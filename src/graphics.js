9d0317d7136f6b4ca5c2ae4628b9b327ab03257b
Vlad Filip <vlad.filip.devl@gmail.com>
2018-10-15 16:19:18 +0300

    added graphics class

---
+++ src/graphics.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const PIXI = require( 'pixi.js' )
+
+module.exports = class Graphics extends PIXI.Graphics {}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 module.exports = {
   Button: require( './button' ),
   Element: require( './element' ),
+  Graphics: require( './graphics' ),
   Scene: require( './scene' ),
   Sprite: require( './sprite' ),
   Stage: require( './stage' ),
